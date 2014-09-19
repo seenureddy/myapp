cf94d2072acf45e416d185539225840dec343eb9
seenureddy <seenu@agiliq.com>
2014-09-19 18:24:38 +0200

    adding databses

--- myapp/config/settings_local.py
+++ myapp/config/settings_local.py
@@ -10,4 +10,14 @@ INSTALLED_APPS += (
     'django_extensions',
 )
 # ALLOWED_HOSTS = ['*']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('databse_name'),
+        'USER': os.environ.get('database_user'),
+        'PASSWORD': os.environ.get('database_password'),
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
 
