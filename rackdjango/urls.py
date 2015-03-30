506f561fca2f7c6690b7c52b277977d99866304e
seenureddy <srinivasulureddy.sura30@gmail.com>
2015-03-30 15:29:33 +0530

    added the rest api and made some database related changes

--- rackdjango/urls.py
+++ rackdjango/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import patterns, include, url
+from rest_framework import routers
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
+
+from appecrest import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet, base_name='users-set')
+router.register(r'groups', views.GroupViewSet, base_name='groups-set')
+
 
 urlpatterns = patterns('',
     # Examples:
@@ -18,6 +26,8 @@ urlpatterns = patterns('',
     url(r'^accounts/',include('registration.backends.default.urls')),
     url(r'$', 'rackdjango.views.home', name='home'),
     url(r'^profile/$', 'rackdjango.views.profile', name='profile'),
-
+    
+    url(r'^', include(router.urls)),
+    url(r'^api_auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 )
