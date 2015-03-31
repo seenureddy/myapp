6623939ab917f9b2fe2e8efb8c824c4157e4f249
seenureddy <srinivasulureddy.sura30@gmail.com>
2015-03-31 16:19:06 +0530

    written the view with mixins and generics

--- appecrest/views.py
+++ appecrest/views.py
@@ -5,10 +5,11 @@ from django.contrib.auth.models import User, Group
 
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.views import APIView
+from rest_framework import mixins, generics
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 from rest_framework.decorators import api_view
-from rest_framework.views import APIView
 from rest_framework.renderers import JSONRenderer
 
 from .models import Snippet
@@ -38,47 +39,32 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json' 
         super(JSONResponse, self).__init__(content, **kwargs)
     
-class SnippetList(APIView):
+class SnippetList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
     """
     List all the code snippet or create new snippet.
     """
-    def get(self, request, format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+    qureyset = Snippet.objects.all()
+    serializers_class = SnippetSerializer
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-    def post(self, request, format=None):
-        serializer = SnippetSerializer(data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTPRESPONSE_201_CREATED)
-        return Response(serializer.error, status=status.HTTPRESPONSE_400_BAD_REQUEST)
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class SnippetDetails(APIView):
+class SnippetDetails(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin,
+    generics.GenericAPIView):
     """ 
     Retrieve, update or delete a code snippet.
     """
-    def get_object(self, pk):
-        try:
-            snippet = Snippet.objects.filter(pk=pk)
-        except Snippet.DoesNotExist:
-            raise Http404
 
-    def get(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+    qureyset = Snippet.objects.all()
+    serializers_class = SnippetSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
     
-    def put(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet, data=request.DATA)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.error, status=status.HTTPRESPONSE_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
     
-    def delete(self, request, pk, format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return HttpResponse(status=HttpResponse_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
