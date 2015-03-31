a416a6f92e129e2d3919847d096626f67fd15168
seenureddy <srinivasulureddy.sura30@gmail.com>
2015-03-31 17:00:04 +0530

    added the user and highlighted

--- appecrest/models.py
+++ appecrest/models.py
@@ -8,10 +8,12 @@ LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
 class Snippet(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100, blank=True, default='')
     code = models.TextField()
+    highlighted = models.TextField()
     linenos = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('auth.User', related_name='snippets')
+    title = models.CharField(max_length=100, blank=True, default='')
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
