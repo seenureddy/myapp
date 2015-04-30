a41e1d380b0c0cbc062619e777d4e611541e5c8c
seenureddy <srinivasulureddy.sura30@gmail.com>
2015-04-30 10:32:39 +0530

    integreating the braintree

--- oldapp/urls.py
+++ oldapp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 from oldapp import views
 
 urlpatterns = patterns('',
-	                   url(r'^$', views.home, name='oldhome'),)
+	                   url(r'^$', views.home, name='oldhome'),
+	                   url(r'^credit/$', views.credit_card, name='credit_form'),)
--- oldapp/views.py
+++ oldapp/views.py
@@ -1,6 +1,12 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response
+
+
 def home(request):
 	context = RequestContext(request)
 	return render_to_response('oldapp/oldhome.html', {}, context)
 
+
+def credit_card(request):
+	context = RequestContext(request)
+	return render_to_response('oldapp/credit_card_form.html', {}, context)
